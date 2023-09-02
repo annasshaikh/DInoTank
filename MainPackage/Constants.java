b9b96c209cc751feac8dcc6b54dba8a78e34ea71
Annas Shaikh <39020847+annasshaikh@users.noreply.github.com>
2023-09-02 12:16:00 +0500

    Decreased Spawn Time

--- MainPackage/Constants.java
+++ MainPackage/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static int HEIGHT = 600;
     public static int NOOFTANK = 3;
     public static int MOVETIME = 100;
-    public static int SPAWNTIME = 3000;
+    public static int SPAWNTIME = 2500;
     public static int BASE = 550;
     public static int LEVELTIME = 1000 * 30;
     public static int Score = 0;
@@ -12,7 +12,7 @@ public class Constants {
 
     public static int Speed = 2;
     public static void LevelUp(){
-        SPAWNTIME = (int)(SPAWNTIME / 1.5);
+        //SPAWNTIME = (int)(SPAWNTIME / 1.5);
         if(level > 0 && level % 3 == 0)
             Speed++;
         level++;
